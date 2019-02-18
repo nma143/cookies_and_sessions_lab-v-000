06e4eac76fc7a34a43411f2a0b56765ceed14ecf
Nikki A <nikki.apostolakis@gmail.com>
2019-02-18 18:36:12 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   get 'sessions/new'
 
-  get '/' => "products#'/'"
+  root 'products#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
